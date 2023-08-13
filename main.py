9fffd52fd23b51ad670804da8403d373784774a1
Demezy <38487319+Demezy@users.noreply.github.com>
2023-08-13 14:02:06 +0300

    aa

---
+++ main.py
@@ -0,0 +1,14 @@
+class Test: 
+    a: int
+    b: str
+    def __init__(self, a:int, b:str):
+        self.a =a
+        self.b = b
+
+
+
+
+
+if __name__ == "__main__":
+    t = Test(1, '2')
+    print(t.a, t.b)
